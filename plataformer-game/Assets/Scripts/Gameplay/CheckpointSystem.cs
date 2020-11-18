e0dc7d0b1867136492258c868059285df867de2e
Luiz Felipe <73553939+FelipeDevMelo@users.noreply.github.com>
2020-11-18 13:54:25 -0300

    Checkpoint animation
    final changes for the checkpoint system

--- plataformer-game/Assets/Scripts/Gameplay/CheckpointSystem.cs
+++ plataformer-game/Assets/Scripts/Gameplay/CheckpointSystem.cs
@@ -7,19 +7,33 @@ public class CheckpointSystem : MonoBehaviour
     /// <summary>
     /// This class Control the checkpoints colliders and 
     /// interection with the player in the scene.
+    /// this contains the animation of the checkpoint and the sound.
     /// </summary>
     /// 
+    private AudioSource audioSrc;
+    public GameObject shinningPRefab;
     private PlayerCheckpoint playerCheckpoint;
-
+    public AudioClip CheckPointSound;
+    IEnumerator waitSecs()
+    {
+        GameObject tempPrefab = Instantiate(shinningPRefab) as GameObject;
+        tempPrefab.transform.position = transform.position;
+        yield return new WaitForSeconds(20f* Time.deltaTime);
+        Destroy(tempPrefab);
+        Destroy(this.gameObject);
+    }//lifeTime of the animation
     private void Start()
     {
+        audioSrc = GetComponent<AudioSource>();
         playerCheckpoint = GameObject.FindGameObjectWithTag("playerCheck").GetComponent<PlayerCheckpoint>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            audioSrc.PlayOneShot(CheckPointSound);
             playerCheckpoint.lastCheckPosition = transform.position;
+            StartCoroutine(waitSecs());
         }
     }
 }
